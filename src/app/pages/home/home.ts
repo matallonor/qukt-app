23e745f43039c2cf1837d02ba5d3de0c5e060727
matallonor <matallonor@gmail.com>
2018-12-06 17:28:36 +0100

    Make home view responsive for desktop browsers

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -3,6 +3,7 @@ import { RecipeProvider } from "app/shared/providers/recipe.provider";
 import {LoadingController, NavController, Platform} from "ionic-angular";
 import { RecipesList } from "app/pages/recipes-list/recipes-list";
 import {TranslateService} from "@ngx-translate/core";
+import {Recipe} from "app/shared/models/recipe";
 
 @Component({
   selector: 'page-home',
@@ -12,46 +13,49 @@ export class HomePage {
 
   private input: string = '';
   private loading: boolean;
-  private animationSrc: string;
+  private recipes: Array<Recipe>;
+  private isBrowser: boolean;
 
   constructor(
     private recipesProvider: RecipeProvider,
     private navCtrl: NavController,
     private translateService: TranslateService,
-    private loader: LoadingController
+    private loader: LoadingController,
+    private platform: Platform
   ) { }
 
-  private search() {
+  private async search() {
     this.loading = true;
-    this.presentLoading();
+    const loader = await this.presentLoading();
     setTimeout(() => {
       this.recipesProvider.getRecipes(this.input).subscribe(
         recipes => {
-          if (recipes) {
-            this.navCtrl.push(HomePage);
+          if (this.platform.is('core') && this.platform.width() > 991) { // If we are on desktop we just load recipes into the template
+            this.recipes = recipes;
+            loader.dismissAll();
+            this.loading = false;
+          } else { // otherwise we navigate to the recipes-list page
+            this.navCtrl.push(RecipesList, { recipes });
           }
-          this.navCtrl.push(RecipesList, { recipes });
-          this.loading = false;
         }, error => {
           console.log('ERROR RETRIEVING RECIPES: ', JSON.stringify(error));
           this.navCtrl.push(HomePage);
         }
       );
-    }, 2000);
+    }, 1000);
   }
 
-  presentLoading() {
-    const loading = this.loader.create({
-      showBackdrop: true,
+  async presentLoading() {
+    const loader = this.loader.create({
       dismissOnPageChange: true,
-      cssClass: 'sa',
       spinner: 'hide',
       content: `
               <div class="custom-spinner-container">
                 <img width="120px" height="120px" src="assets/imgs/loading.gif" />
               </div>`
     });
-    loading.present();
+    loader.present();
+    return loader;
   }
 
 }
